e201d06fcbf73171d17c8df2571051e30667af42
Zedrick <inedc4zedrickr@gmail.com>
2018-10-28 18:13:29 -0600

    url allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'zedrickr.pythonanywhere.com']
