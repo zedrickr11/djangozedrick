86d8c9bd72ee260c7afd90a776deddad381e9020
Zedrick <inedc4zedrickr@gmail.com>
2018-10-29 00:14:51 -0600

    login

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -12,10 +13,8 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,6 +28,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -43,15 +43,18 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path, include
+from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
+
 urlpatterns = [
+    #url(r'^accounts/login/$', views.login, name='login'),
+    #url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+
     path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+
     path('', include('blog.urls')),
 ]
